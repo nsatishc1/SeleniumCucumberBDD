28f033e3d91bdcc75caaad9de0f6a43708833b4d
Satish <satish.nandivelugu@gmail.com>
2024-06-30 13:24:12 +0530

    sample program

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,7 @@ import static java.lang.Math.sqrt;
 
 public class Main {
     public static void main(String[] args) {
-    int num = 4, counter = 0;
-    for(int iter=2;iter<=(sqrt(num));iter++){
-        if(num%iter == 0){
-            counter++;
-            break;
-        }
-
-    }
-        if(counter>0){
-            System.out.println(num+" is not prime");
-        }
-        else{
-            System.out.println(num+" is prime");
-        }
+        System.out.println("sample");
 
     }
 }
