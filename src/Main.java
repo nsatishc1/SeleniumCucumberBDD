aecf0b52491188fb84eaf53668e368a0b5c8225d
Satish <satish.nandivelugu@gmail.com>
2024-06-30 13:10:33 +0530

    Prime Number - most advanced logic - need to check only till 'n/2' rather than to loop till 'n'

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static void main(String[] args) {
-    int num = 18, counter = 0;
-    for(int iter=2;iter<num/2;iter++){
+    int num = 4, counter = 0;
+    for(int iter=2;iter<=(sqrt(num));iter++){
         if(num%iter == 0){
             counter++;
             break;
